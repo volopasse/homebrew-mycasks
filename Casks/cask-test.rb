048bac92ad8ee0a8c6bfa9c2aebdb6de864058f4
Denis Kalugin <denis.kalugin@gmail.com>
2014-07-28 21:41:24 +0100

    added test cask

---
+++ Casks/cask-test.rb
@@ -0,0 +1,10 @@
+class Cask-test < Cask
+  version 'latest'
+  sha256 ':nocheck'
+
+  url "https://transmission.cachefly.net/Transmission-#{version}.dmg"
+  appcast 'http://update.transmissionbt.com/appcast.xml'
+  homepage 'http://www.transmissionbt.com/'
+
+  link 'Cask-test.app'
+end
